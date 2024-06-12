d81a32467e82f69bc7321b5e244f8fa17a5743fb
UNNNYOU <unnnyou@gmail.com>
2024-06-12 19:26:17 +0900

    [add] omniauthでのデータの取得とjwtでのトークン作成
    omniauthでのデータの取得とjwtでのトークン作成の処理を作成しました。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,28 @@
 class SessionsController < ApplicationController
-  def new
-    render :new
-  end
-
   def create
     user_info = request.env['omniauth.auth']
-    raise user_info # Your own session management should be placed here.
+
+    uid_info = user_info['uid']
+    provider_info = user_info['provider']
+    github_id_info = user_info['info']['nickname']
+    token_info = token_generator(uid_info, provider_info)
+
+    user_auth = UserAuthentications.find_by(uid: uid_info, provider: provider_info)
+
+    if user_auth
+      redirect_to "#{ENV['FRONT_URL']}?token=#{user_auth.token}"
+    else
+      user = User.create(name: 'GRASS BATTLE MEMBER', github_id: github_id_info)
+      user_auth = UserAuthentications.create(user_id: user.id, uid: uid_info, provider: provider_info, token: token_info)
+      redirect_to "#{ENV['FRONT_URL']}?token=#{user_auth.token}"
+    end
+  end
+
+  private
+
+  def token_generator(uid_info, provider_info)
+    exp_info = Time.now.to_i + 24 * 3600
+    payload = { github_id: uid_info, provider: provider_info, exp: exp_info }
+    JWT.encode(payload, ENV['SECRET_KEY_BASE'], 'HS256')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # githubからのリダイレクトを受け取るためのルーティング
+  get '/auth/:provider/callback', to: 'sessions#create'
 
   # Defines the root path route ("/")
   # root "articles#index"
