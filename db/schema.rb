11c1cf98f7d13553eb7fe46891000bc9c667ec40
UNNNYOU <unnnyou@gmail.com>
2024-06-14 11:49:36 +0900

    [add] ユーザー及び認証モデルの作成
    作成にあたってはomniauthで取得できる必要最低限の情報を追加しています。

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_12_140327) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_13_002708) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "user_authentications", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["uid"], name: "index_user_authentications_on_uid", unique: true
+    t.index ["user_id"], name: "index_user_authentications_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "GRASS BATTLE MEMBER", null: false
@@ -22,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_12_140327) do
     t.index ["github_id"], name: "index_users_on_github_id", unique: true
   end
 
+  add_foreign_key "user_authentications", "users"
 end
