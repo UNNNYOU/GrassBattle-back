ade2a991c0e7dcccddf2663c65dc71540bf442e3
UNNNYOU <unnnyou@gmail.com>
2024-06-19 14:42:43 +0900

    [add] homeへの変更に伴う処理
    mypageからhomeに変更したことによる処理を行いました。

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  def mypage
+  def home
     user_status = UserStatus.find_by(user_id: @current_user.id)
     render json: { current_user: @current_user, user_status: user_status }, status: :ok
   end
